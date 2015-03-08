907a827d625701dc9da7c31196c02e1f42273c2a
Colby Skeggs <skeggsc@catlin.edu>
2015-03-07 23:45:48 -0800

    Added awards and refactored a bit.

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import webapp2, os, cgi, datetime, sys, time, logging, json
 #  static_dir: static
 #  secure: always
 
+files = {"/": "index.html", "/awards": "awards.html", "/ccre": "ccre.html", "/style.css": "style.css"}
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers["Content-Type"] = "text/html"
-		with open("index.html", "r") as f:
+		path = self.request.path
+		assert path in files, "Bad path (somehow?): %s" % path
+		self.response.headers["Content-Type"] = "text/css" if path.endswith(".css") else "text/html"
+		with open(files[path], "r") as f:
 			while True:
 				x = f.read(4096)
 				if not x: break
 				self.response.write(x)
 
 application = webapp2.WSGIApplication([
-	('/', MainPage)
+	(key, MainPage)
+    for key in files.keys()
 ])
